be33ef3c96947a9ccb07b8034773c1c3e2613591
bryanesteban <bryanesteban_123@hotmail.com>
2024-04-11 16:11:21 -0500

    service hero

--- src/app/heroes/pages/list-page/list-page.component.ts
+++ src/app/heroes/pages/list-page/list-page.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../../interfaces/hero.interface';
+import { HeroesService } from '../../services/heroes.services';
+
 
 @Component({
   selector: 'app-list-page',
   templateUrl: './list-page.component.html',
   styles: ``
 })
-export class ListPageComponent {
+export class ListPageComponent implements OnInit {
+
+  public heroes : Hero[] =[];
+
+  constructor(private HeroesService: HeroesService) {}
+
+
+  ngOnInit(): void {
+    this.HeroesService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes);
+  }
+
+
+
+
 
 }
