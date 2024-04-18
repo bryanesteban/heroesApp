114a2cc75438795d2121af51c83de41fb8da7eb9
bryanesteban <bryanesteban_123@hotmail.com>
2024-04-17 23:07:26 -0500

    changes in services

--- src/app/auth/guards/auth.guard.ts
+++ src/app/auth/guards/auth.guard.ts
@@ -1,11 +1,24 @@
-import { Injectable } from '@angular/core';
-import { CanActivateFn, CanMatch, CanMatchFn } from '@angular/router';
+import { Injectable, inject } from '@angular/core';
+import { CanActivateFn, CanMatchFn, Router, UrlTree } from '@angular/router';
+import { tap, Observable, take } from 'rxjs';
+import { AuthService } from '../services/auth.service';
 
 @Injectable({providedIn: 'root'})
-export class AuthGuard  implements CanMatchFn, CanActivateFn{
-
-
-
-  constructor() { }
-
+export class AuthGuard {
 }
+
+const isAuthenticated = (): | boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+  return authService.checkAuthentication().Pipe(
+      take(1),
+      tap((isAuthenticated: boolean) => {
+          if (!isAuthenticated) {
+              router.navigate(['./auth/login']);
+          }
+      }),
+  );
+}
+
+export const canActivateGuard:CanActivateFn = isAuthenticated;
+export const canMatchGuard:CanMatchFn = isAuthenticated;
